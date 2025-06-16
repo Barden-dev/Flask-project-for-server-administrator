725eef758229bd8a0d65a2148baa9f6e8e9cf8c0
Barden-dev <barden2410@gmail.com>
2025-06-16 13:14:43 +0300

    Add hello.html, and test projetct

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from os import name
-from flask import Flask
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Helllo world'
+def main():
+    return render_template('hello.html')
 
-@app.route('/some')
-def some_page():
-    return 'Some Page'
+@app.route('/submit', methods=['POST'])
+def submit():
+  name = request.form['name']
+  return f'Hello, {name}'
 
 if __name__ == '__main__':
     app.run(debug=True)
