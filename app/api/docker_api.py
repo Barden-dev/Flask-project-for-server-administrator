88356914ada2ea47e9251330f4f014f5689f5663
Barden-dev <barden2410@gmail.com>
2025-06-17 23:06:55 +0300

    Made control for docker containers, simple, but it works

--- app/api/docker_api.py
+++ app/api/docker_api.py
@@ -1,5 +1,48 @@
-from flask import Blueprint
+import docker
+from flask import Blueprint, jsonify, request
+from numpy import mat
 
 # Создаем "чертеж" для Docker API
 docker_bp = Blueprint('docker_api', __name__, url_prefix='/api/v1/docker')
+client = docker.from_env()
 
+@docker_bp.route('/containers')
+def get_containers():
+    end_data = {}
+    containers = client.containers.list()
+    for container in containers:
+        end_data[container.short_id] = (container.name, container.status, container.id)
+    
+    return jsonify(end_data)
+
+
+@docker_bp.route('/containers/<id>/<action>', methods=['POST'])
+def container_action(id, action):
+    name = id
+    action = action
+    
+    if action == "":
+        return "Bad POST, no action"  
+    
+    match action:
+        case "start":
+            try:
+                client.containers.run(str(name))
+            except:
+                return "Error"
+            return "Started"
+        
+        case "stop":
+            try:
+                client.containers.get(name).stop()
+            except:
+                return "Error"
+            return "Stoped"
+        
+        case "restart":
+            try:
+                client.containers.get(name).restart()
+            except:
+                return "Error"
+            return "restarted"
+    return "OK"    
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -4,6 +4,102 @@ function updateProgressBar(value) {
     progressBar.style.width = value + '%';
 }
 
+function get_containers(){
+  const html_container = document.getElementById('items_container');
+  const items = [];
+    fetch('http://localhost:5000/api/v1/docker/containers')
+    .then(response => response.json())
+    .then(data => {
+        html_container.innerHTML = '';
+        const containers = Object.entries(data).map(([id, container_data]) => ({
+        id: id,
+        name: container_data[0],
+        status: container_data[1],
+        full_id: container_data[2]
+        }));
+
+        containers.forEach(item => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+          <input type="hidden" id="${item["id"]}" name="items" value="${item["id"]}">
+          <label for="${item["id"]}"><span class="container-id">${item["id"]}</span></label>
+          <button onclick="showContainerDetails(${JSON.stringify(item).replace(/"/g, '&quot;')})">Свойства</button>
+        `;
+        if (!document.getElementById(item["id"])){
+          html_container.appendChild(div);
+        }
+      });
+    });  
+}
+
+function showContainerDetails(containerData) {
+  const modal = document.getElementById('containerModal');
+  const details = document.getElementById('containerDetails');
+  
+  details.innerHTML = `
+    <p><strong>Id:</strong> ${containerData.id}</p>
+    <p><strong>Название:</strong> ${containerData.name}</p>
+    <p><strong>Статус:</strong><span id="current-status" class="status status-${containerData.status}"> ${containerData.status}</span></p>
+    <button onclick="containerAction('${containerData.id}', 'start')">Start</button>
+    <button onclick="containerAction('${containerData.id}', 'stop')">Стоп</button>
+    <button onclick="containerAction('${containerData.id}', 'restart')">Рестарт</button>
+  `;
+  modal.style.display = 'block';
+}
+
+
+
+function containerAction(containerId, action) {
+  const statusSpan = document.getElementById('current-status');
+   if (action === 'start') {
+        statusSpan.textContent = 'starting...';
+    } else if (action === 'stop') {
+        statusSpan.textContent = 'stopping...';
+    } else if (action === 'restart') {
+        statusSpan.textContent = 'restarting...';
+    }
+  statusSpan.className = 'status status-loading';
+  
+  fetch(`http://localhost:5000/api/v1/docker/containers/${containerId}/${action}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      container_id: containerId
+    })
+  })
+
+  .then(response => {
+    if (response.ok) {
+      console.log(`${action} выполнен успешно`);
+      get_containers();
+    } else {
+      console.error('Ошибка:', response.status);
+    }
+  })
+
+  .then(data => {
+    let newStatus;
+    if (action === 'start' || action === 'restart') {
+            newStatus = 'running';
+        } else if (action === 'stop') {
+            newStatus = 'stopped';
+        }
+        statusSpan.textContent = newStatus;
+        statusSpan.className = `status status-${newStatus}`;
+    if (currentContainerData && currentContainerData.id === containerId) {
+          currentContainerData.status = newStatus;}
+  })
+
+  .catch(error => console.error('Ошибка запроса:', error));
+}
+
+function getSelectedItems() {
+  const checkboxes = document.querySelectorAll('input[name="items"]:checked');
+  return Array.from(checkboxes).map(cb => cb.value);
+}
+
 
 let cpu_data = [];
 function get_system_info(){
@@ -30,8 +126,7 @@ function get_system_info(){
     console.log("Возникла проблема с api-запросом: ", error.message);
     }
 }
-get_system_info()
-let update_info = setInterval(() => get_system_info(), 1000)
+
 
 const ctx = document.getElementById('myChart');
 const chart = new Chart(ctx, {
@@ -66,7 +161,7 @@ const chart = new Chart(ctx, {
     },
 });
 
-let update_chart = setInterval(() => chart.update(), 1000)
+
 
 
 document.getElementById("scriptForm").addEventListener("submit", async (e) => {
@@ -91,3 +186,10 @@ document.getElementById("scriptForm").addEventListener("submit", async (e) => {
       alert("Ошибка при отправке запроса: " + error.message);
     }
   });
+
+
+get_containers();
+get_system_info()
+let update_info = setInterval(() => get_system_info(), 1000);
+let update_chart = setInterval(() => chart.update(), 1000);
+let update_containers = setInterval(() => get_containers(), 1000);
